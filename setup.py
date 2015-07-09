ccb5474a59979ba0f23c534bb5fc3318e2138b42
Zakir Durumeric <zakird@gmail.com>
2015-07-09 10:45:09 -0400

    adding a basic egg definition

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name = "zson",
+    description = "A JSON serializer for Kombu (and therefore also Celery) that "
+                  "supports encoding objects by defining to_json and from_json methods.",
+    version = "0.0.1",
+    license = "Apache License, Version 2.0",
+    author = "Zakir Durumeric",
+    author_email = "zakird@gmail.com",
+    maintainer = "Zakir Durumeric",
+    maintainer_email = "zakird@gmail.com",
+
+    keywords = "python json schema bigquery elastic search",
+
+    packages = [
+        "zschema"
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'zschema = zschema.cli:main',
+        ]
+    }
+}
+
