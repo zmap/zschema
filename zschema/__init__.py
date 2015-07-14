c7eb7001f661a75bb2ef12e40e1b85b51c057c7b
Zakir Durumeric <zakird@gmail.com>
2015-07-14 15:23:35 -0400

    schema registration

--- zschema/__init__.py
+++ zschema/__init__.py
@@ -14,10 +14,19 @@ from zschema.leaves import (
     Integer,
     IPv4Address
 )
-
-
 from zschema.compounds import ListOf, SubRecord, Record
 
-__version__ = "0.0.5"
+__version__ = "0.0.9"
 
 #__all__ = ["keys", "leaves", "compounds", "tests"]
+
+__schemas = {}
+
+def register_schema(name, schema):
+    if name in __schemas:
+        raise Exception("Name conflict. %s already registered in global namespace." % name)
+    else:
+        __schemas[name] = schema
+
+def get_schema(name):
+    return __schemas[name]
