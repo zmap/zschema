3737f1c1c80f5b42e3fae96caf9f33a9860a1a30
Zakir Durumeric <zakird@gmail.com>
2017-08-31 16:28:56 -0400

    bump revno for improved validation

--- zschema/__init__.py
+++ zschema/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "ZMap Team"
 __email__ = "zmap-team@umich.edu"
 __copyright__ = "Copyright 2015 Regents of the Unversity of Michigan"
-__version__ = "0.7.0"
+__version__ = "0.8.0"
 __license__ = "Apache License, Version 2.0"
 
 __all__ = ["keys", "leaves", "compounds", "registry"]
