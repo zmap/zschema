1bd27a67461d217be2cf367271f7c31e2fa21fe3
Zakir Durumeric <zakird@gmail.com>
2017-09-13 17:30:30 -0400

    def meant 1752

--- zschema/leaves.py
+++ zschema/leaves.py
@@ -421,7 +421,7 @@ class DateTime(Leaf):
     VALID = "Wed Jul  8 08:52:01 EDT 2015"
     INVALID = "Wed DNE  35 08:52:01 EDT 2015"
 
-    MIN_VALUE = "1582-01-01 00:00:00.000000 EDT"
+    MIN_VALUE = "1752-01-01 00:00:00.000000 EDT"
     MAX_VALUE = "9999-12-31 00:00:00.000000 EDT"
 
     def __init__(self, *args, **kwargs):
