19053e670aa51d9e880ac7c3e1ef3be85a42ac4a
Zakir Durumeric <zakird@gmail.com>
2018-03-29 10:05:37 -0400

    updating copyright date

--- zschema/__init__.py
+++ zschema/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "ZMap Team"
 __email__ = "team@zmap.io"
-__copyright__ = "Copyright 2015 Regents of the Unversity of Michigan"
+__copyright__ = "Copyright 2018 Regents of the Unversity of Michigan"
 __version__ = "0.9.0"
 __license__ = "Apache License, Version 2.0"
 
