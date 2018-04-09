zmap/zschema

commit 4ff784676ffeef7c30dbc45b4d866baabdc56da6

Fix BigQuery schema compilation test (#22)
The test failure was because the BigQuery schema contains lists which zschema
may generate in a different order. My fix adds a specialized assertion method
to assert that two BigQuery schemas are equivalent.

https://github.com/zmap/zschema/pull/22

zschema/tests.py
@@ -114,11 +114,31 @@ VALID_BIG_QUERY = [
         "type": "INTEGER",
         "name": "ip",
         "mode": "NULLABLE"
-    }
+    },
 ]
 
 
 class CompileAndValidationTests(unittest.TestCase):
+
+    def assertBigQuerySchemaEqual(self, a, b):
+        """
+        Assert that the given BigQuery schemas are equivalent.
+
+        BigQuery schemas consist of lists whose order doesn't matter, dicts,
+        and privimites.
+        """
+        if type(a) is not type(b):
+            return False
+        if hasattr(a, '__len__') and hasattr(b, '__len__'):
+            self.assertEquals(len(a), len(b))
+        if isinstance(a, list):    
+            for x, y in zip(sorted(a), sorted(b)):
+                self.assertBigQuerySchemaEqual(x, y)
+        elif isinstance(a, dict):
+            for k in a:
+                self.assertBigQuerySchemaEqual(a[k], b[k])
+        else:
+            return self.assertEquals(a, b)
 
     def setUp(self):
         self.maxDiff=10000
@@ -141,7 +161,7 @@ class CompileAndValidationTests(unittest.TestCase):
     def test_bigquery(self):
         global VALID_BIG_QUERY
         r = self.host.to_bigquery()
-        self.assertEqual(r, VALID_BIG_QUERY)
+        self.assertBigQuerySchemaEqual(r, VALID_BIG_QUERY)
 
     def test_elasticsearch(self):
         global VALID_ELASTIC_SEARCH

commit 22baab5c8529c8f1838bc912d8d8ec48c228a864

Assert rather than returning from BigQuery schema assertion method
This fixes the method introduced in #22.

zschema/tests.py
@@ -127,8 +127,7 @@ class CompileAndValidationTests(unittest.TestCase):
         BigQuery schemas consist of lists whose order doesn't matter, dicts,
         and privimites.
         """
-        if type(a) is not type(b):
-            return False
+        self.assertEquals(type(a), type(b))
         if hasattr(a, '__len__') and hasattr(b, '__len__'):
             self.assertEquals(len(a), len(b))
         if isinstance(a, list):    
@@ -138,7 +137,7 @@ class CompileAndValidationTests(unittest.TestCase):
             for k in a:
                 self.assertBigQuerySchemaEqual(a[k], b[k])
         else:
-            return self.assertEquals(a, b)
+            self.assertEquals(a, b)
 
     def setUp(self):
         self.maxDiff=10000