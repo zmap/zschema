08b226fd3a28700bec24acef547f1a8d059e53e5
Zakir Durumeric <zakird@gmail.com>
2018-05-01 08:38:01 -0400

    Remove print statements

--- zschema/tests.py
+++ zschema/tests.py
@@ -839,7 +839,4 @@ class ValidationPolicies(unittest.TestCase):
                 }
             }
         }
-        print schema.definition["445"].validation_policy
-        print schema.definition["445"].definition["smb"].validation_policy
-
         self.assertRaises(DataValidationException, lambda: schema.validate(doc))
