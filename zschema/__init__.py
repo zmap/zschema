a4ebfb9774502252b8eeefbeba2460fbc8f0993b
David Adrian <davidcadrian@gmail.com>
2018-05-11 15:24:31 -0400

    Bump revno 0.9.1

--- zschema/__init__.py
+++ zschema/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "ZMap Team"
 __email__ = "team@zmap.io"
 __copyright__ = "Copyright 2018 Regents of the Unversity of Michigan"
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __license__ = "Apache License, Version 2.0"
 
 __all__ = ["keys", "leaves", "compounds", "registry"]
